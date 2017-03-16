bbb03da269803f44fea67bc485eaa9834fc71985
Raphael ATALLAH <raphael@noemail.fr>
2017-03-16 15:16:06 +0100

    Updated index

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get('/', function (req, res) {
 
 app.post('/api/invoice', upload.single('file'), function (req, res, next) {
     console.log('Received a file', req.file, req.body);
-    fs.rename(req.file.path, req.file.path + '.jpg', (err) => {
+    fs.rename(req.file.path, req.file.path + '.jpg', function (err) {
         invoices.push({
             file: req.file.filename + '.jpg',
             number: req.body.number,
@@ -29,6 +29,6 @@ app.post('/api/invoice', upload.single('file'), function (req, res, next) {
 })
 
 var port = process.env.PORT || 5500;
-app.listen(, function () {
+app.listen(function () {
     console.log('Example app listening on port ' + port + '!')
 })
